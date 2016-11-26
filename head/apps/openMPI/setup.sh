f6b99cca8634421481d8618528230a264b3b24c3
Banshee1221 <Banshee1221@gmail.com>
2016-11-26 18:36:26 +0200

    simple cleanup

--- head/apps/openMPI/setup.sh
+++ head/apps/openMPI/setup.sh
@@ -5,7 +5,7 @@ GLOBAL_VER="v2.0"
 OMPI_VER="2.0.1"
 OMPI_PKG="openmpi-$OMPI_VER"
 OMPI_EXT="tar.gz"
-URL=https://www.open-mpi.org/software/ompi/$GLOBAL_VER/downloads/$OMPI_PKG.$OMPI_EXT
+URL="https://www.open-mpi.org/software/ompi/$GLOBAL_VER/downloads/$OMPI_PKG.$OMPI_EXT"
 
 
 # Get the packages
