9937e9059ac966042d5098d5d7d1be6f78822314
Banshee1221 <Banshee1221@gmail.com>
2016-11-27 15:46:47 +0200

    cleanup installer script

--- installer.sh
+++ installer.sh
@@ -3,16 +3,21 @@
 # the host since you actually want to manage the clients through ansible
 # playbooks, sicne you can just easily execute that from the host.
 
+helpPrinter () {
+  printf "\n\tThis script starts the installer for either the headnode
+\tor the compute nodes in the cluster and must be executed
+\ton each of the respective nodes individually. It takes
+\tonly one argument.\n\n\tUsage:
+\t  ./installer.sh --head\t\tThis will configure the head node.
+\t  ./installer.sh --compute\tThis will configure the compute node.\n\n"
+  }
+
 VAL=0
 for var in "$@"
 do
   if [[ $var == "-h" ]] || [[ $var == "--help" ]]
   then
-    printf "\n\tThis script starts the installer for either the headnode
-\tor the compute nodes in the cluster and must be executed
-\ton each of the respective nodes individually.\n\n\tUsage:
-\t  ./installer.sh --head:\tThis will configure the head node.
-\t  ./installer.sh --compute:\tThis will configure the compute node.\n\n"
+    helpPrinter
     exit 0
   else
     ((VAL++))
@@ -21,7 +26,7 @@ done
 
 if [ $VAL -gt 1 ]
 then
-  echo "Error: Don't pass more than one argument"
+  helpPrinter
   exit 1
 fi
 
@@ -40,6 +45,18 @@ do
           cd -
           exit 0
           ;;
+      --*)
+        helpPrinter
+        exit 1
+        ;;
+      -*)
+        helpPrinter
+        exit 1
+        ;;
+      *)
+        helpPrinter
+        exit 1
+        ;;
   esac
   shift
 done
