c687c1cecb7265b66e7bafb70daa265fd340770b
Banshee1221 <Banshee1221@gmail.com>
2016-11-27 19:13:15 +0200

    removed unnecessary post folder from head

--- head/post/network/setup.sh
+++
@@ -1,4 +0,0 @@
-### Copy over hosts configuration to compute nodes and set correct nameserver
-ansible nodes -m copy -a "src=/etc/hosts dest=/etc/hosts"
-ansible nodes -m shell -a "echo 'nameserver $STATIC_IP_HEAD' > /etc/resolv.conf"
-ansible nodes -m shell -a "chattr +i /etc/resolv.conf"
