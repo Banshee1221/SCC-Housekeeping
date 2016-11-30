33da1f77dddb6fa559110ae4e48d0d372241d0de
Eugene de Beste <banshee1221@gmail.com>
2016-11-30 18:52:47 +0200

    fix bash syntax fail

--- comp_apps/gadget2/fftw.sh
+++ comp_apps/gadget2/fftw.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-FFTW_VER = "2.1.5"
-FFTW_PKG = "fftw"
-FFTW_EXT = "tar.gz"
-FFTW_PKG_NAME = $FFTW_PKG-$FFTW_VER
-FFTW_PKG_FULL = $FFTW_PKG_NAME.$FFTW_EXT
-FFTW_URL = "http://www.fftw.org/$FFTW_PKG_FULL"
+FFTW_VER="2.1.5"
+FFTW_PKG="fftw"
+FFTW_EXT="tar.gz"
+FFTW_PKG_NAME=$FFTW_PKG-$FFTW_VER
+FFTW_PKG_FULL=$FFTW_PKG_NAME.$FFTW_EXT
+FFTW_URL="http://www.fftw.org/$FFTW_PKG_FULL"
 
 wget $FFTW_URL
 tar xfz $FFTW_PKG_FULL
--- comp_apps/gadget2/gsl.sh
+++ comp_apps/gadget2/gsl.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-GSL_VER = "1.9"
-GSL_PKG = "GSL"
-GSL_EXT = "tar.gz"
-GSL_PKG_NAME = $GSL_PKG-$GSL_VER
-GSL_PKG_FULL = $GSL_PKG_NAME.$GSL_EXT
-GSL_URL = "http://mirror.rit.edu/gnu/GSL/$GSL_PKG_FULL"
+GSL_VER="1.9"
+GSL_PKG="GSL"
+GSL_EXT="tar.gz"
+GSL_PKG_NAME=$GSL_PKG-$GSL_VER
+GSL_PKG_FULL=$GSL_PKG_NAME.$GSL_EXT
+GSL_URL="http://mirror.rit.edu/gnu/GSL/$GSL_PKG_FULL"
 
 
 wget $GSL_URL
